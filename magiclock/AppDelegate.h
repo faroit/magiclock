89ce85371c6c8dfeacfceef0fabee8ab436da9e7
Fabian-Robert Stöter <mail@faroit.com>
2016-05-26 13:50:57 +0200

    cleanup

--- magiclock/AppDelegate.h
+++ magiclock/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  feel-the-clock
-//
-//  Created by Fabian-Robert Stöter on 15/04/16.
-//  Copyright © 2016 Fabian-Robert Stöter. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
