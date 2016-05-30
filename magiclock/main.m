68db7473e6d134267fb73feb32d5ffdfa787878e
Fabian-Robert Stöter <mail@faroit.com>
2016-05-30 09:11:27 +0200

    remove code header

--- magiclock/main.m
+++ magiclock/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  feel-the-clock
-//
-//  Created by Fabian-Robert Stöter on 15/04/16.
-//  Copyright © 2016 Fabian-Robert Stöter. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
